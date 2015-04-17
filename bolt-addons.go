7c4607c89bc0be9826bb0c19789b8e52135fd5f1
ifn <shabaev.shat@gmail.com>
2015-04-17 10:42:26 +0300

    added conf

--- bolt-addons.go
+++ bolt-addons.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/BurntSushi/toml"
 	"github.com/boltdb/bolt"
 	"github.com/codegangsta/cli"
 	"github.com/gorilla/mux"
@@ -48,24 +50,42 @@ func CreateBucketHandler(bs *BoltServer) HandlerFunc {
 
 //
 
-type BoltServer struct {
-	port string
-	db   *bolt.DB
+type Conf struct {
+	Port int
+
+	FilePath string
+	FileMode string
 }
 
-func NewBoltSrv(port string) *BoltServer {
-	bs := new(BoltServer)
-	bs.port = port
-	return bs
+type BoltServer struct {
+	port string
+
+	filePath string
+	fileMode os.FileMode
+
+	db *bolt.DB
+}
+
+func NewBoltSrv(conf Conf) (bs *BoltServer, err error) {
+	bs = new(BoltServer)
+
+	bs.port = strconv.Itoa(conf.Port)
+	bs.filePath = conf.FilePath
+	fileMode, err := strconv.ParseUint(conf.FileMode, 8, 32)
+	if err != nil {
+		return
+	}
+	bs.fileMode = os.FileMode(fileMode)
+
+	return
 }
 
 func (self *BoltServer) Start() error {
-	db, err := bolt.Open("my.db", 0600, nil)
+	db, err := bolt.Open(self.filePath, self.fileMode, nil)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
-
 	self.db = db
 
 	r := mux.NewRouter()
@@ -75,25 +95,33 @@ func (self *BoltServer) Start() error {
 	return http.ListenAndServe(":"+self.port, nil)
 }
 
-func startBoltSrv(port string) error {
-	b := NewBoltSrv(port)
+func startBoltSrv(confPath string) (err error) {
+	var conf Conf
+	if _, err = toml.DecodeFile(confPath, &conf); err != nil {
+		return
+	}
+
+	b, err := NewBoltSrv(conf)
+	if err != nil {
+		return
+	}
 
 	return b.Start()
 }
 
 func main() {
 	app := cli.NewApp()
-	app.Name = ""
-	app.Usage = "bolt http server"
+	app.Name = "bwa"
+	app.Usage = "bolt web api"
 	app.Version = "0.0.1"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "port, p",
-			Value: "3344",
+			Name:  "config, conf, cfg, c",
+			Value: "conf.toml",
 		},
 	}
 	app.Action = func(c *cli.Context) {
-		err := startBoltSrv(c.String("port"))
+		err := startBoltSrv(c.String("config"))
 		if err != nil {
 			log.Fatal(err)
 		}
